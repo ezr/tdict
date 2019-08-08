eba7d2e9db8297eef1decfae42df97b14ffe0218
ezr <ezr@users.noreply.github.com>
2019-08-08 00:13:51 -0500

    add initial code to the project

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # tdict
-A simple offline dictionary for the terminal
+A simple offline dictionary for the terminal. 
+Just give it the word you want to look up as an argument.
+
+The program expects the definition files from [here](https://github.com/tusharlock10/Dictionary/blob/master/data.7z) to be in `/usr/share/tdict/`
+
+The directory should look like this:
+`$ ls /usr/share/tdict/
+DA.json  DD.json  DG.json  DJ.json  DM.json  DP.json  DS.json  DV.json  DY.json
+DB.json  DE.json  DH.json  DK.json  DN.json  DQ.json  DT.json  DW.json  DZ.json
+DC.json  DF.json  DI.json  DL.json  DO.json  DR.json  DU.json  DX.json`
---
+++ tdict.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+    "encoding/json"
+    "fmt"
+    "io/ioutil"
+    "log"
+    "os"
+    "reflect"
+    "strings"
+)
+
+func handleError(e error) {
+    if e != nil {
+        log.Fatal(e)
+    }
+}
+
+type Dictionary struct {
+    Entries map[string]json.RawMessage
+}
+
+type Entry struct {
+    Antonyms []string `json:"ANTONYMS"`
+    Synonyms []string `json:"SYNONYMS"`
+    Meanings map[string]interface{} `json:"MEANINGS"`
+}
+
+func meaningPrinter(t interface{}) {
+    s := reflect.ValueOf(t)
+    fmt.Println(s.Index(0), "|", s.Index(1))
+}
+
+func listPrinter(class string, l []string) {
+    if len(l) == 0 {
+        return
+    }
+    fmt.Printf("%s: ", class)
+    for i, word := range l {
+        if (i + 1) == len(l) {
+            fmt.Printf("%s\n", word)
+        } else {
+            fmt.Printf("%s, ", word)
+        }
+    }
+}
+
+func main() {
+    if len(os.Args) < 2 {
+        fmt.Println("Usage:", os.Args[0], "<word>")
+        os.Exit(1)
+    }
+    arg := strings.ToUpper(os.Args[1])
+    datadir := "/usr/share/tdict/"
+    raw, err := ioutil.ReadFile(datadir + "D" + string(arg[0]) + ".json")
+    handleError(err)
+    var d map[string]*json.RawMessage
+    json.Unmarshal([]byte(raw), &d)
+
+    if _, ok := d[arg]; ok {
+        var e Entry
+        json.Unmarshal(*d[arg], &e)
+
+        for _, v := range e.Meanings {
+            meaningPrinter(v)
+        }
+        listPrinter("SYNONYMS", e.Synonyms)
+        listPrinter("ANTONYMS", e.Antonyms)
+    } else {
+        fmt.Println(os.Args[1], "not found")
+    }
+}
